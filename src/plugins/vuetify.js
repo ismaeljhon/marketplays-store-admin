f8870ac69b86a2e59f4a72235c1a296ab3ba71af
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-08-29 17:35:49 +0800

    Add missing material icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,28 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import MaterialIcon from '@/components/MaterialIcon'
+
+function missingMaterialIcons(ids) {
+    const icons = {}
+    for (const id of ids) {
+      for (const suffix of ['fill', 'outline', 'two-tone', 'round', 'sharp']) {
+        const name = `${id}_${suffix}`
+        icons[name] = {
+          component: MaterialIcon,
+          props: {
+            name
+          }
+        }
+      }
+    }
+    return icons
+}
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'md',
+        ...missingMaterialIcons(['visibility', 'visibility_off'])
+    },
 });
