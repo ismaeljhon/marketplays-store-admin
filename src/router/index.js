8efda1ea59039d33f49211b7a150766bf72c8af6
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-10-20 09:44:30 +0800

    add env to ignored files, update document page title

--- src/router/index.js
+++ src/router/index.js
@@ -41,4 +41,9 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  document.title = to.name + " - " + process.env.VUE_APP_NAME
+  next()
+})
+
 export default router
